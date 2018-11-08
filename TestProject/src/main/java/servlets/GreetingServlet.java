7aaf2e0d4d742c7b4ba2ed9a6336f36ad086fb11
Somsurya Nanda <somsuryananda@gmail.com>
2018-11-08 16:42:35 +0530

    TestProject initial commit.

---
+++ TestProject/src/main/java/servlets/GreetingServlet.java
@@ -0,0 +1,32 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GreetingServlet
+ */
+public class GreetingServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public GreetingServlet() {
+		super();
+		System.out.println("Inside greeting servlet !! ");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException 
+	{
+		response.getWriter().println("Hello World from Servlet");		
+	}
+
+}
---
+++ TestProject/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World! from JSP</h2>
+</body>
+</html>
