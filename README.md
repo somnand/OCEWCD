8ef785fa5219d1c70f42fa09f657e252260f9769
somnand <somsuryananda@gmail.com>
2019-01-02 17:38:09 +0530

    Added more HTTP methods (viz. GET,POST,DELETE). Utilized the InitParams for generating the messages to be shown.

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # OCEWCD
 Storage for all J2EE Experiments
 
-* [TestProject](https://github.com/somnand/OCEWCD/tree/development/TestProject) - Working copy for J2EE Programs. branch suppported is /development.
+* [TestProject](https://github.com/somnand/OCEWCD/tree/development/TestProject) - Working copy for J2EE Programs. branch supported is /development.
+* [TestProject3.0](https://github.com/somnand/OCEWCD/tree/development/TestProject3.0) - Similar to TestProject but works on the principle of 3.0 Servlet format.It doesn't contain a web.xml rather all components are marked using annotations. Branch supported is /development.
 * JSPTestingProject - Legacy project for testing J2EE Programs.**(present below commit f0f34a64c123f68f97df5a4c085d67279dea0210)**
 * LoginApp-MVC - Modified J2EE Programming platform. It is transformed into a maven project. Individual README.md has more information.**(present below commit f0f34a64c123f68f97df5a4c085d67279dea0210)**
 * Mavenized-LoginApp-MVC -  Same as bove but this is created using maven. **Currently decommissioned due to some unkown issue in the deployment phase.** **(present below commit f0f34a64c123f68f97df5a4c085d67279dea0210)**
--- TestProject3.0/src/servlets/HelloServlet.java
+++ TestProject3.0/src/servlets/HelloServlet.java
@@ -2,6 +2,8 @@ package servlets;
 
 import java.io.IOException;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,17 +12,37 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloServlet
  */
-@WebServlet(urlPatterns= {"/hello","/helloAnnotation"})
+@WebServlet(
+urlPatterns= {"/hello","/helloAnnotation"},
+initParams= {
+		@WebInitParam(name="getMessage",value="Hello World - GET"),
+		@WebInitParam(name="postMessage",value="Hello World - POST"),
+		@WebInitParam(name="deleteMessage",value="DONOT delete any resource on the machine!!!")
+			}
+)
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
+	String getMsg,postMsg,deleteMsg;
+	
+	public void init(ServletConfig config)
+	{
+		 getMsg=config.getInitParameter("getMessage");
+		 postMsg=config.getInitParameter("postMessage");
+		 deleteMsg=config.getInitParameter("deleteMessage");
+	}	
 	public void doGet(HttpServletRequest request,HttpServletResponse response)throws IOException
 	{
-		response.getWriter().println("Hello World - GET");
+		response.getWriter().println(getMsg);
 	}
 	
 	public void doPost(HttpServletRequest request,HttpServletResponse response)throws IOException
 	{
-		response.getWriter().println("Hello World - POST");
+		response.getWriter().println(postMsg);
+	}
+	
+	public void doDelete(HttpServletRequest request,HttpServletResponse response)throws IOException
+	{
+		response.getWriter().println(deleteMsg);
 	}
 }
